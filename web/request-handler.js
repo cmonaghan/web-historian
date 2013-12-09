a5891af1eb13b69cb2cf8ecb935134c7eb56988d
Christian Monaghan <christian.monaghan@gmail.com>
2013-12-09 15:33:11 -0800

    Server responds to GET requests

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,6 +1,16 @@
 var path = require('path');
+var httpHelpers = require('./http-helpers.js');
+
 module.exports.datadir = path.join(__dirname, "../data/sites.txt"); // tests will need to override this.
 
+var status = 200;
+
 module.exports.handleRequest = function (req, res) {
-  console.log(exports.datadir);
+  console.log("Serving request type " + req.method + " for url " + req.url);
+  var data = exports.datadir;
+
+  res.writeHead(status, httpHelpers.headers);
+  res.end('/<input/');
+
+  console.log("res._data is ", res._data);
 };
